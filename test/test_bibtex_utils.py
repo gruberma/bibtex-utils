2adde3355e3f6349cf8411b39a69c1ffc7d5d201
Martin Gruber <martin.gruber1@web.de>
2025-01-07 00:58:30 +0100

    Fix CI jobs under windows

--- test/test_bibtex_utils.py
+++ test/test_bibtex_utils.py
@@ -1,7 +1,10 @@
+# pylint: disable=missing-module-docstring
+
 from bibtex_utils import get_all_cites_in_dir
 
 
 def test_get_all_cites_in_dir():
+    """Test cites extraction from multiple files in a dictionary"""
     cites = get_all_cites_in_dir("test/resources")
     assert cites == {
         "gruber2021empirical",
